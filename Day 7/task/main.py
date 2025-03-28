a487b16ce10da2fa1bc55a3bb00cc25ea4f77904
programflow <kevinfloort@gmail.com>
2025-03-28 17:29:50 -0400

    Day6 and Day7

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,8 +1,70 @@
-word_list = ["aardvark", "baboon", "camel"]
+import random
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+import hangman_words
+import hangman_art
+word_list = hangman_words.word_list
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+lives = 6
+
+
+print(hangman_art.logo)
+
+chosen_word = random.choice(word_list)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+
+
+    print(f"****************************<???>/{lives} LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+
+    guessed_letters = ""
+    if guess not in guessed_letters:
+        guessed_letters += guess
+
+        display = ""
+
+        for letter in chosen_word:
+            if letter == guess:
+                display += letter
+                correct_letters.append(guess)
+            elif letter in correct_letters:
+                display += letter
+            else:
+                display += "_"
+
+        print("Word to guess: " + display)
+
+
+
+        if guess not in chosen_word:
+            lives -= 1
+            print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
+            if lives == 0:
+                game_over = True
+
+
+                print(f"***********************YOU LOSE**********************")
+                print(f"The answer is {chosen_word}")
+                exit()
+
+        if "_" not in display:
+            game_over = True
+            print("****************************YOU WIN****************************")
+            exit()
+
+        # TODO-2: - Update the code below to use the stages List from the file hangman_art.py
+        print(hangman_art.stages[lives])
+    else:
+        print(f"You've already guessed {guess}")
