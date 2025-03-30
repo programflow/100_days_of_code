61300fe1585ee507d4b09e7b070edce2f67ff6b3
programflow <kevinfloort@gmail.com>
2025-03-30 16:59:29 -0400

    Day9

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,29 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+import art
+
+print(art.logo)
+print("Welcome to the secret auction program.")
+bids = {}
+keep_bidding = "yes"
+
+while keep_bidding == "yes":
+    bidder = input("What is your name?: ")
+    bid = input("What's Your bid?: $")
+
+    bids[bidder] = int(bid)
+
+    keep_bidding = input("Are there any other bidders? Type 'yes' or 'no'.")
+
+
+highest_bid =0
+highest_bidder = None
+
+for name in bids:
+
+    if bids[name] > highest_bid:
+        highest_bid = bids[name]
+        highest_bidder = name
+
+print(f"The winner is {highest_bidder} with a d bid of ${highest_bid}")
+
 
 
