6c04f00cf58c8c1b8feba2d564f2a7201c9a7131
programflow <kevinfloort@gmail.com>
2025-04-18 17:26:38 -0400

    Day 24 mail merg: used list comprehension to strip the list of names. and seperate the for loop from some of the with as statements

--- Day 24/Mail Merge Project Start/main.py
+++ Day 24/Mail Merge Project Start/main.py
@@ -1,28 +1,19 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 letters = []
 with open("./Input/Names/invited_names.txt", mode='r') as name_file:
-    names = name_file.readlines()
-    for name in names:
-        with open("./Input/Letters/starting_letter.txt") as letter_file:
-            stripped_name =name.strip()
-            letter_template = letter_file.readlines()
+    names = [name.strip() for name in name_file.readlines()]
 
-            letter_template[0] = letter_template[0].replace("[name]", stripped_name)
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_template = letter_file.read()
 
-            letter = "".join(letter_template)
-
-            with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.docx", "a") as invitations:
-                invitations.write(letter)
-
-
-
-
-
-
+for name in names:
+    letter = letter_template.replace("[name]", name)
+    with open(f"./Output/ReadyToSend/letter_for_{name}.txt", "a") as invitations:
+        invitations.write(letter)
