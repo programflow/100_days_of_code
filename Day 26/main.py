e6b7643c089bff7f2b0cc1cf428259cc255846ac
programflow <kevinfloort@gmail.com>
2025-04-20 19:26:56 -0400

    cleaned up some code with list comprehensions in us_states_game/main.py

---
+++ Day 26/main.py
@@ -0,0 +1,9 @@
+# new_list = [new_item for item in list]
+
+name = "Kevin"
+new_list = [letter for letter in name]
+print(new_list)
+
+# Conditional List Comprehension
+# new_list = [new_item for item in list if test]
+
