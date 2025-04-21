ffaf2e04632f3284e69fbd9503660e09ea4f19f0
programflow <kevinfloort@gmail.com>
2025-04-20 20:08:08 -0400

    Day 26 complete (nato alphabet converter): main.py

--- Day 26/main.py
+++ Day 26/main.py
@@ -7,3 +7,29 @@ print(new_list)
 # Conditional List Comprehension
 # new_list = [new_item for item in list if test]
 
+# Dictionary Comprehension
+# new_dict = {new_key:new_value for item in list}
+# new_dict = {new_key:new_value for (key,value) in dict.items()}
+
+student_dict = {
+    "student" :["Angela", "James", "Lily"],
+    "score" : [56, 79, 98]
+}
+
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    print(value)
+
+import pandas
+
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
+
+#Loop through a data frame
+# for (key, value) in student_data_frame.items():
+#     print(value )
+
+#Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    if row.student == "Angela":
+        print(row.score)
