e4aadafdccd1f992723c745f0bf1ef7973411b52
programflow <kevinfloort@gmail.com>
2025-04-24 21:59:38 -0400

    Day 28 Complete

--- Day 28/main.py
+++ Day 28/main.py
@@ -2,35 +2,69 @@ import tkinter
 import time
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
-DARKPURPLE = "#3D365C".lower()
-BLUE = "#5F99AE".lower()
-PINKPURPLE = "#C95792".lower()
+DARKPURPLE = "#3D365C"
+BLUE = "#5F99AE"
+PINKPURPLE = "#C95792"
 ORANGECREAM = "#F8B55F"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-TIMER = WORK_MIN*60
-REPS = 2
+
+
+fg_reset = PINKPURPLE
+fg_rest = ORANGECREAM
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text,text="00:00")
+    global reps
+    reps = 0
+    check_mark_label.config(text= "")
+    timer_label.config(text= "Timer", fg=fg_reset)
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    global TIMER
-    countdown(TIMER)
+
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        timer_label.config(text="Long Break", fg=fg_rest)
+
+        countdown(long_break_sec)
+    elif reps % 2 == 0:
+        timer_label.config(text="Break", fg=fg_rest)
+        countdown(short_break_sec)
+
+    else:
+        timer_label.config(text="Work",fg=BLUE)
+        countdown(work_sec)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def countdown(count):
+    global timer
     minutes = count // 60
     seconds = count % 60
     new_time = f"{minutes:02}:{seconds:02}"
     canvas.itemconfig(timer_text, text=new_time)
     if count > 0:
-        window.after(1000, countdown, count -1)
-
+        timer =window.after(1000, countdown, count -1)
+    else:
+        start_timer()
+        if reps % 2 == 0:
+            check_mark_label["text"] = check_mark_label["text"] + "✔"
 
 
 
@@ -39,7 +73,7 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=150, pady=50, bg=DARKPURPLE)
 
-fg = BLUE
+fg = PINKPURPLE
 
 
 canvas = Canvas(width=200, height=224, bg=DARKPURPLE,highlightthickness=0)
@@ -49,16 +83,16 @@ timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT
 canvas.grid(column=1, row=1)
 
 
-timer_label = Label(text="Timer", fg=fg, bg=DARKPURPLE, font=(FONT_NAME, 40, "bold"))
+timer_label = Label(text="Timer", fg=fg_reset, bg=DARKPURPLE, font=(FONT_NAME, 40, "bold"))
 timer_label.grid(column=1, row=0)
 
 start_button = Button(text="Start", command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_mark_label = Label(text = "✅", fg = BLUE, bg=DARKPURPLE, font=(FONT_NAME, 30, "bold"))
+check_mark_label = Label(text = "", fg = BLUE, bg=DARKPURPLE, font=(FONT_NAME, 30, "bold"), highlightthickness=0)
 check_mark_label.grid(column=1, row=3)
 
 window.mainloop()
--- Day 29/main.py
+++ Day 29/main.py
@@ -1,13 +1,46 @@
 from tkinter import *
+from tkinter import messagebox
 
-
+def clear_entries():
+    website_entry.delete(0, END)
+    email_entry.delete(0, END)
+    password_entry.delete(0, END)
+    website_entry.focus()
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def add_password_data():
+
+def save():
     website =website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    is_ok = messagebox.askokcancel(title=website, password=password)
+    new_login_data = f"{website} | {email} | {password}\n"
+
+    if not login_already_saved(new_login_data.strip()):
+
+
+
+
+        with open("login_data.txt", "a") as file:
+            file.write(new_login_data)
+        clear_entries()
+        messagebox.showinfo(title="Success", message="Login Successfully saved")
+    else:
+        messagebox.showerror(title="Error", message="Login already saved")
+
+
+# ---------------------------- CHECK IF PASSWORD DATA ALREADY EXISTS ------------------------------- #
+def login_already_saved(new_login_data):
+    with open("login_data.txt", "r") as file:
+        login_info = file.readlines()
+
+    for login in login_info:
+        if login.strip() == new_login_data:
+            return True
+    return False
+
 
 
 
@@ -46,7 +79,7 @@ password_entry.grid(row=3, column=1, sticky="w")
 #Buttons
 generate_password_button = Button(text="Generate Password", justify="left")
 generate_password_button.grid(row=3, column=2, sticky="w")
-add_button = Button(width=41, text="Add", justify="left")
+add_button = Button(width=41, text="Add", justify="left", command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="w")
 
 window.mainloop()
