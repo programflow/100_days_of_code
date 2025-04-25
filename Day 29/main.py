b9927c71edb48906d9a982bc979914797aeae194
programflow <kevinfloort@gmail.com>
2025-04-25 18:13:21 -0400

    Day 30 goal complete: Edit password manager to catch file not found errors and change it to handle json filestypes in stead of txt

--- Day 29/main.py
+++ Day 29/main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from random import randint, choice, shuffle
 import os
 import pyperclip
+import json
 
 
 
@@ -42,40 +43,61 @@ def save():
     website =website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-
     if valid_entries():
+        new_login = {
+            website: [
+                {
+                "email": email,
+                "password": password
+            }
+            ]
+        }
 
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"These are the details entered: \nEmail: {email} \nPassword: {password}")
-
-        new_login_data = f"{website} | {email} | {password}\n"
-        if is_ok:
-            if not os.path.exists("login_data.txt") or not login_already_saved(new_login_data.strip()):
-
-
-
-
-                with open("login_data.txt", "a") as file:
-                    file.write(new_login_data)
-                clear_entries()
+        try:
+            with open('login_data.json', 'r') as data_file:
+                data = json.load(data_file)
+                data.update(new_login)
+            with open('login_data.json', 'w') as file:
+                json.dump(data, file, indent=4)
                 messagebox.showinfo(title="Success", message="Login Successfully saved")
-            else:
-                messagebox.showerror(title="Error", message="Login already saved")
+        except:
+            messagebox.showinfo("New file Created", "New File Created")
+            with open('login_data.json', 'w') as file:
+                json.dump(new_login, file, indent=4)
+                messagebox.showinfo(title="Success", message="Login Successfully saved")
+
+
+
+        clear_entries()
+        # else:
+        #     messagebox.showerror(title="Error", message="Login already saved")
     else:
         print(valid_entries())
         messagebox.showerror(title="Error", message="Please don't leave any fields empty!")
 
 
-# ---------------------------- Login ALready Saved ------------------------------- #
 
-def login_already_saved(new_login_data):
-    with open("login_data.txt", "r") as file:
-        login_info = file.readlines()
+# -------------------------------Search for Existing Logins----------------------------- #
+def search():
+    try:
+        with open('login_data.json', 'r') as data_file:
+            data = json.load(data_file)
+            website = website_entry.get()
+            if website in data:
+                print(f"You have the following Login information for {website}:")
+                message = f"You have the following Login information for {website}:"
+                for i in range(len(data[website])):
+                    email = data[website][i]['email']
+                    password = data[website][i]['password']
+                    message += f"\n{email}: {password}"
 
-    for login in login_info:
-        if login.strip() == new_login_data:
-            return True
-    return False
+                messagebox.showinfo(title="Search Result", message=message)
+            else:
+                messagebox.showinfo(title="No Search results",message=f"{website} login doesn't exist")
+    except:
+        messagebox.showinfo(title="No Search results",message=f"{website} login doesn't exist")
+
+
 
 # -------------------------------Check for Empy Entries----------------------------- #
 
@@ -116,7 +138,7 @@ password_label = Label(text="Password:", fg="black", bg="white")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=44, justify="left")
+website_entry = Entry(width=24, justify="left")
 website_entry.grid(row=1, column=1, columnspan=2, sticky="w")
 website_entry.insert(0, "")
 website_entry.focus()
@@ -128,6 +150,8 @@ password_entry.grid(row=3, column=1, sticky="w")
 password_entry.insert(0, "")
 
 #Buttons
+search_button = Button(width=16, text="Search", justify="left", command=search)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", justify="left", command=generate_new_password)
 generate_password_button.grid(row=3, column=2, sticky="w")
 add_button = Button(width=41, text="Add", justify="left", command=save)
